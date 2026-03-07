a2bbe32c0ab7275dddab5bb5f17837e4992abf87
Abiola Malik <abiolamalik30@gmail.com>
2026-03-07 16:07:01 +0100

    first pr action

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log("Hello, world! This is a GitHub Action written in TypeScript.");
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
+async function run() {
+    const token = (0, core_1.getInput)("gh-token");
+    const label = (0, core_1.getInput)("label");
+    const octokit = (0, github_1.getOctokit)(token);
+    const { pull_request } = github_1.context.payload;
+    if (!pull_request) {
+        (0, core_1.setFailed)("This action can only be run on pull_request events.");
+        return;
+    }
+    const { owner, repo } = github_1.context.repo;
+    const prNumber = pull_request.number;
+    try {
+        await octokit.rest.issues.addLabels({
+            owner,
+            repo,
+            issue_number: prNumber,
+            labels: [label],
+        });
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            (0, core_1.setFailed)(`Failed to add label: ${error.message}`);
+        }
+        else {
+            (0, core_1.setFailed)(`Failed to add label: ${error}`);
+        }
+    }
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello, world! This is a GitHub Action written in TypeScript.");
+import { setFailed, getInput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+async function run() {
+  const token = getInput("gh-token");
+  const label = getInput("label");
+  const octokit = getOctokit(token);
+  const { pull_request } = context.payload;
+  if (!pull_request) {
+    setFailed("This action can only be run on pull_request events.");
+    return;
+  }
+  const { owner, repo } = context.repo;
+  const prNumber = pull_request.number;
+  try {
+    await octokit.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number: prNumber,
+      labels: [label],
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      setFailed(`Failed to add label: ${error.message}`);
+    } else {
+      setFailed(`Failed to add label: ${error}`);
+    }
+  }
+}
+
+run();
